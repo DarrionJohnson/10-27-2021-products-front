928bb1c057282b3616fefe4b5982aed723240a75
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-10-27 20:37:09 -0500

    Connect it to root form html.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("hiss");
+// To select the root div from index.html
+const root = document.querySelector("#root");
+
+// Create a txt on the html.
+root.innerHTML = `<p class= "text-center">Hello World!</p>`;
