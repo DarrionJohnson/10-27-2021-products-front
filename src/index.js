42a94d65840933ceaaf125888bc27139089b1f2e
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-11-03 20:29:30 -0500

    Code updates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
+import Forms from "./componets/Forms";
+import List from "./componets/List";
 import api from "./services/api.services";
 
 // To select the root div from index.html
 const root = document.querySelector("#root");
 
-api.index().then((data) => {
-  console.log(data);
+api.index().then((products) => {
+  root.innerHTML = products.map((product) => `<p>${product.name}</p>`).join("");
+});
+
+api.indx().then((products) => {
+  root.innerHTML = `
+  <main class="container mx-auto flex flex-col items-center">
+    ${Forms}
+    ${List(products)}
+  </main>
+`;
 });
 
 // Create a txt on the html.
-root.innerHTML = `<p class= "text-center">Hello World!</p>`;
+// root.innerHTML =
+//   <main class="container mx-auto flex flex-col items-center">
+//     ${Forms}
+//     ${List(products)}
+//   </main>
+// `;
